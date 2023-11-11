cf5867597d7cef4b7b77df33faa6206947be4c3d
AlexHorodnic <alexhorodnic@protonmail.com>
2023-11-11 10:07:38 +0200

    Add routing, prettier formatter, and modify the app component html content.

--- employee-management-frontend/src/app/app.component.ts
+++ employee-management-frontend/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {EmployeeListComponent} from "./employee-list/employee-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, EmployeeListComponent, RouterLink],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'employee-management';
