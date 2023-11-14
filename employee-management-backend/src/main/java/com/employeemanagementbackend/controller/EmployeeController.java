9e3a686f33a449368805448d6230fc7b8f4f0256
AlexHorodnic <alexhorodnic@protonmail.com>
2023-11-14 07:40:22 +0200

    update base url inside employee service.

--- employee-management-backend/src/main/java/com/employeemanagementbackend/controller/EmployeeController.java
+++ employee-management-backend/src/main/java/com/employeemanagementbackend/controller/EmployeeController.java
@@ -19,13 +19,6 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    // Controller for Testing.
-        @GetMapping("/employees/hello-world")
-        public String hello() {
-            return "hello world!";
-        }
-
-
     //get all employees
     @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
