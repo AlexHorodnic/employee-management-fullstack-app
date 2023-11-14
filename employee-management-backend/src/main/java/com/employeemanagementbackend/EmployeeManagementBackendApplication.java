59fa2c01913ea505531753a950a6c79099ec7d0f
AlexHorodnic <alexhorodnic@protonmail.com>
2023-11-14 05:44:50 +0200

    change configuration.

--- employee-management-backend/src/main/java/com/employeemanagementbackend/EmployeeManagementBackendApplication.java
+++ employee-management-backend/src/main/java/com/employeemanagementbackend/EmployeeManagementBackendApplication.java
@@ -2,10 +2,12 @@ package com.employeemanagementbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class EmployeeManagementBackendApplication {
 
+	@Profile({"dev","prod"})
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementBackendApplication.class, args);
 	}
